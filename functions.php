4c60157ae65ba34d44fb8eae6e20b601b1ba06c1
Vincent Rozenberg <vincentrozenberg@gmail.com>
2017-07-10 09:21:00 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function set_avatar_based_on_first_letter( $avatar, $params ) {
+  $user_id = $params['item_id'];
+  $name_field = 1; //profile field id
+
+    $name = xprofile_get_field_data(  $name_field, $user_id ); //get the profile field
+    $name = substr($name, 0, 1); //get the first letter
+    $name = strtolower( $name ); //make lowercase
+
+        $avatar = get_bloginfo('template_url') . '/images/avatar/'.$name.'.png'; //a.png, b.png, etc.
+
+    return $avatar;
+}
+add_filter( 'bp_core_default_avatar_user', 'set_avatar_based_on_first_letter', 10, 2 );
